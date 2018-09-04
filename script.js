b1a712c467a5ae2b70335ff3870162c70897d5e7
Clinton Johnson <dragoon1246224@gmail.com>
2018-09-04 12:43:55 -0400

    Almost done

---
+++ script.js
@@ -0,0 +1,35 @@
+const coin = {
+    state: 0,
+    flip: function() {
+        this.state = (Math.floor(Math.random() * 2));
+    },
+    toString: function() {
+        if (this.state === 0) {
+           console.log("Heads")
+        }
+        else if (this.state === 1) {
+            console.log("Tails")
+        }
+    },
+    toHTML: function() {
+        const image = document.createElement('img');
+        if (this.state === 0) {
+            image.classList.add("heads");
+            document.body.appendChild(image);
+            
+        }
+        else if (this.state === 1) {
+            image.classList.add("tails");
+            document.body.appendChild(image);
+        }
+        // 3. One point: Set the properties of this image element to show either face-up
+        //    or face-down, depending on whether this.state is 0 or 1.
+        return image;
+    }
+};
+
+for (let i = 0; i < 20; i++) {
+coin.flip()
+coin.toHTML()
+coin.toString()
+}
